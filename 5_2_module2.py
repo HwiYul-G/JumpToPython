8c4a9ab0985e9e9445cb78b30556fe5ce8c63e7f
Y <hwiyul_g@daum.net>
2023-02-03 18:44:36 +0900

    Doing module

---
+++ 5_2_module2.py
@@ -0,0 +1,18 @@
+# 클래스나 변수 등을 포함한 모듈
+PI = 3.141592
+
+class Math:
+    def solv(self, r):
+        return PI * (r ** 2)
+
+def add(a, b):
+    return a + b
+
+"""
+import mod2
+print(mod2.PI)
+a = mod2.Math()
+print(a.solv(2))
+
+print(mod2.add(mod2.PI, 4.4))
+"""
