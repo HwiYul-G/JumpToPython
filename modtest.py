266faf0a85628c43c4c50b737cccbcc75fb21dd2
Y <hwiyul_g@daum.net>
2023-02-03 20:47:52 +0900

    module

---
+++ modtest.py
@@ -0,0 +1,11 @@
+import mod2
+result = mod2.add(3, 4)
+print(result)
+
+# 모듈을 불러오는 또 다른 방법
+# 지금까지 해당 모듈이 있는 디렉터리로 이동한 후에 그 모듈을 사용할 수 있었다.
+# 이번엔 모듈을 저장한 디렉터리로 이동하지 않고 모듈을 불러와 사용하는 방법을 보자.
+
+# mymod directory를 만들고 mod2.py를 mymod directory로 이동시킨다.
+# mkdir mymod
+# move mod2.py mymod
